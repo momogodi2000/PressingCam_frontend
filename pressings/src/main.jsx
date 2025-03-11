974086ee438a0ecc61777e96b60de56e450175c4
momogodi2000 <yvangodimomo@gmail.com>
2025-03-11 12:30:17 +0100

    first commit

--- pressings/src/main.jsx
+++ pressings/src/main.jsx
@@ -1,21 +1,20 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
-import LandingPage from './components/landing_page/LandingPage.jsx'
-import Authentication from './components/Authentication/auth.jsx'
+import './index.css'; // Add this line
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App.jsx';
+import LandingPage from './components/landing_page/LandingPage.jsx';
+import Authentication from './components/Authentication/auth.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />}>
-          {/* Public routes */}
-          <Route index element={<LandingPage />} /> {/* Route for the landing page */}
-          <Route path="/login" element={<Authentication />} /> {/* Route for login page */}
+          <Route index element={<LandingPage />} />
+          <Route path="/login" element={<Authentication />} />
         </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
-)
+);
